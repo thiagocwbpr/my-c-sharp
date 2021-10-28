544b229f16e26ddfc6ebb75b2c7b09c70f868d4a
Thiago Menezes <thiagocwbpr@gmail.com>
2021-10-28 14:13:36 -0300

    Started project, showing the Date Time in two colors, black and green, only test.

---
+++ Estudo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Estudo
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            DateTime n1 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime n2 = DateTime.Parse("2000-08-20T13:05:58Z");
+
+            Console.WriteLine("n1: "+ n1);
+            Console.WriteLine("n2: "+ n2);
+      
+        }
+    }
+}
